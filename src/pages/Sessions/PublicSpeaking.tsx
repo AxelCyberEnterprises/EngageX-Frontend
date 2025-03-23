176ce6da22d582c297be30efe7a7a5da521f6c8f
hoodiedan <obodedaniel3@gmail.com>
2025-03-23 19:35:24 +0100

    Update session components for improved styling and progress bar color

--- src/pages/Sessions/PublicSpeaking.tsx
+++ src/pages/Sessions/PublicSpeaking.tsx
@@ -7,19 +7,15 @@ import PublicSpeakingTimer from "@/components/session/PublicSpeakingTimer";
 import VideoStreamer from "@/components/session/RecordView";
 import EngagementMetrics from "@/components/session/EngagementMetrics";
 import { useNavigate } from "react-router-dom";
-import {
-    Dialog,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogDescription,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog";
 import alert from "../../assets/images/svgs/alert.svg";
 
 const PublicSpeaking: React.FC = () => {
     const [stop, setStop] = useState(false);
     const [startTimer, setStartTimer] = useState(false);
     const [isDialogOneOpen, setDialogOneOpen] = useState(false);
+    const [isDialogTwoOpen, setDialogTwoOpen] = useState(false);
+    // const [isQuestionDialogOpen, setQuestionDialogOpen] = useState(false);
     const navigate = useNavigate();
 
     const StopStreaming = () => {
@@ -31,39 +27,41 @@ const PublicSpeaking: React.FC = () => {
         <div className="text-primary-blue">
             <section className="flex flex-wrap border-b-1 border-bright-gray px-8 py-4 justify-between items-center">
                 <div className="w-full">
-                    <h4 className="mb-4">Public Speaking Session</h4>
+                    <div className="flex justify-between items-center">
+                        <h4 className="mb-0 md:mb-4">Public Speaking Session</h4>
+
+                        <Button
+                            className="bg-jelly-bean hover:bg-jelly-bean/90 flex my-4 md:hidden"
+                            onClick={() => setDialogOneOpen(true)}
+                        >
+                            <SquareArrowUpRight className="me-1" /> End Session
+                        </Button>
+                    </div>
                     <div className="mb-3">
                         <PublicSpeakingTimer minutes={2} start={startTimer} />
                     </div>
                 </div>
             </section>
 
-            <section className="flex flex-wrap">
+            <section className="flex flex-wrap md:flex-row-reverse">
                 {/* left side  */}
-                <div className="w-full md:w-9/12 lg:w-3/12 md:px-8 lg:pe-4 py-4">
+                <div className="w-full hidden md:block gap-4 md:w-3/12 md:px-8 lg:pe-4 py-4">
                     <VoiceAnalytics percent1={72} percent2={62} percent3={85} />
-                    
+
                     <EngagementMetrics percent1={32} percent2={42} percent3={35} />
                 </div>
 
                 {/* middle side  */}
-                <div className="w-full md:w-9/12 lg:w-6/12 md:px-8 lg:pe-4 py-4">
+                <div className="w-full md:w-9/12 lg:w-6/12 md:px-8 lg:pe-4 py-4 px-4">
                     <div className="">
                         <div className="rounded-xl w-full h-120">
-                            <Button
-                                className="bg-jelly-bean hover:bg-jelly-bean/90 flex my-4 md:hidden"
-                                onClick={() => setDialogOneOpen(true)}
-                            >
-                                <SquareArrowUpRight className="me-1" /> End Session
-                            </Button>
-
                             <h6 className="mb-3">Live Audience</h6>
                             <img src={audience} alt="audience" className="w-full h-full object-cover rounded-xl" />
                         </div>
 
                         <div className="w-full flex justify-end mt-16 px-4 md:px-0">
                             <Button
-                                className="bg-jelly-bean hover:bg-jelly-bean/90 flex"
+                                className="bg-jelly-bean hover:bg-jelly-bean/90 hidden md:flex"
                                 onClick={() => setDialogOneOpen(true)}
                             >
                                 <SquareArrowUpRight className="me-1" /> End Session
@@ -72,8 +70,8 @@ const PublicSpeaking: React.FC = () => {
                                 <DialogContent>
                                     <DialogHeader>
                                         <img src={alert} alt="green image of users" className="w-16 h-16 mb-4" />
-                                        <DialogTitle className="text-primary-blue">End a session</DialogTitle>
-                                        <DialogDescription className="text-auro-metal-saurus">
+                                        <DialogTitle className="text-primary-blue text-left">End a session</DialogTitle>
+                                        <DialogDescription className="text-auro-metal-saurus text-left">
                                             You are about to End a Session, you will be sent to the Session Analysis
                                             Report page and you will no longer be able to continue this session
                                         </DialogDescription>
@@ -99,12 +97,12 @@ const PublicSpeaking: React.FC = () => {
                 </div>
 
                 {/* right side large screens  */}
-                <div className="right__side hidden md:block w-full md:w-3/12 lg:w-3/12 px-8 lg:ps-4 py-4">
-                    <div className="py-5 px-3 border-1 border-bright-gray rounded-xl">
+                <div className="right__side w-full hidden lg:block md:w-3/12 lg:w-3/12 px-8 lg:ps-4 py-4">
+                    <div className="py-5 px-3 hidden md:block border-1 border-bright-gray rounded-xl">
                         <VideoStreamer
                             duration={2}
                             stop={stop}
-                            onStop={StopStreaming}
+                            onStop={() => setDialogTwoOpen(true)}
                             onStart={() => setStartTimer(true)}
                         />
                     </div>
@@ -114,19 +112,38 @@ const PublicSpeaking: React.FC = () => {
                         <p>This is a note</p>
                     </div>
                 </div>
+                <Dialog open={isDialogTwoOpen} onOpenChange={setDialogTwoOpen}>
+                    <DialogContent>
+                        <DialogHeader>
+                            <img src={alert} alt="green image of users" className="w-16 h-16 mb-4" />
+                            <DialogTitle className="text-primary-blue text-left">Session ended</DialogTitle>
+                            <DialogDescription className="text-auro-metal-saurus text-left">
+                                Looks like you ran out of time, kindly proceed by clicking next to view your result
+                            </DialogDescription>
+                        </DialogHeader>
+                        <div className="w-full">
+                            <Button
+                                className="bg-primary-blue hover:bg-primary-blue/90 w-full"
+                                onClick={() => StopStreaming()}
+                            >
+                                Next
+                            </Button>
+                        </div>
+                    </DialogContent>
+                </Dialog>
 
                 {/* right side mobile */}
-                <div className="p-4 md:hidden w-full">
-                    <div className="border-1 border-gray py-5 px-3">
+                <div className="px-4 md:hidden w-full mb-5 ">
+                    <div className="border-1 border-gray py-5 px-3 rounded-md">
                         <div className="row flex">
                             <div className="w-6/12">
-                                <h6>Live Audience Reaction</h6>
+                                <h6>Voice Analytics</h6>
                             </div>
 
                             <div className="flex flex-col justify-between items-center w-2/12">
                                 <div className="flex items-center justify-center bg-cultured w-1/2 aspect-square rounded-4xl mb-2">
-                                    <div className="w-6 h-6 bg-primary-blue rounded-4xl flex items-center justify-center bg-">
-                                        <ThumbsUp className="w-1/2 aspect-square text-white" />
+                                    <div className="w-6 h-6 bg-bright-gray rounded-4xl flex items-center justify-center bg-">
+                                        <ThumbsUp className="w-1/2 aspect-square text-primary-blue" />
                                     </div>
                                 </div>
                                 <p>{86}</p>
@@ -134,8 +151,8 @@ const PublicSpeaking: React.FC = () => {
 
                             <div className="flex flex-col justify-between items-center w-2/12">
                                 <div className="flex items-center justify-center bg-cultured w-1/2 aspect-square rounded-4xl mb-2">
-                                    <div className="w-6 h-6 bg-primary-blue rounded-4xl flex items-center justify-center bg-">
-                                        <ThumbsUp className="w-1/2 aspect-square text-white" />
+                                    <div className="w-6 h-6 bg-bright-gray rounded-4xl flex items-center justify-center bg-">
+                                        <ThumbsUp className="w-1/2 aspect-square text-primary-blue" />
                                     </div>
                                 </div>
                                 <p>{86}</p>
@@ -143,8 +160,40 @@ const PublicSpeaking: React.FC = () => {
 
                             <div className="flex flex-col justify-between items-center w-2/12">
                                 <div className="flex items-center justify-center bg-cultured w-1/2 aspect-square rounded-4xl mb-2">
-                                    <div className="w-6 h-6 bg-primary-blue rounded-4xl flex items-center justify-center bg-">
-                                        <ThumbsUp className="w-1/2 aspect-square text-white" />
+                                    <div className="w-6 h-6 bg-bright-gray rounded-4xl flex items-center justify-center bg-">
+                                        <ThumbsUp className="w-1/2 aspect-square text-primary-blue" />
+                                    </div>
+                                </div>
+                                <p>{86}</p>
+                            </div>
+                        </div>
+                        <div className="row flex mt-3">
+                            <div className="w-6/12">
+                                <h6>Engagement Metrics</h6>
+                            </div>
+
+                            <div className="flex flex-col justify-between items-center w-2/12">
+                                <div className="flex items-center justify-center bg-cultured w-1/2 aspect-square rounded-4xl mb-2">
+                                    <div className="w-6 h-6 bg-bright-gray rounded-4xl flex items-center justify-center bg-">
+                                        <ThumbsUp className="w-1/2 aspect-square text-primary-blue" />
+                                    </div>
+                                </div>
+                                <p>{86}</p>
+                            </div>
+
+                            <div className="flex flex-col justify-between items-center w-2/12">
+                                <div className="flex items-center justify-center bg-cultured w-1/2 aspect-square rounded-4xl mb-2">
+                                    <div className="w-6 h-6 bg-bright-gray rounded-4xl flex items-center justify-center bg-">
+                                        <ThumbsUp className="w-1/2 aspect-square text-primary-blue" />
+                                    </div>
+                                </div>
+                                <p>{86}</p>
+                            </div>
+
+                            <div className="flex flex-col justify-between items-center w-2/12">
+                                <div className="flex items-center justify-center bg-cultured w-1/2 aspect-square rounded-4xl mb-2">
+                                    <div className="w-6 h-6 bg-bright-gray rounded-4xl flex items-center justify-center bg-">
+                                        <ThumbsUp className="w-1/2 aspect-square text-primary-blue" />
                                     </div>
                                 </div>
                                 <p>{86}</p>
