de9a5905f97b4976ac3d19912b6e107b6ad88779
Abdul <abdulmusty03@outlook.com>
2025-03-28 11:24:00 +0100

    style: address goals section discrepancies

--- src/config/form-field-options.ts
+++ src/config/form-field-options.ts
@@ -1,6 +1,5 @@
 import ConferenceRoom from "@/assets/images/pngs/conference-room.png";
 
-export const DefaultGoals = [{ id: 1, goal: "" }];
 
 export const VirtialEnvironmentOptions = [
     { name: "Conference Room", value: "conference_room", src: ConferenceRoom },
