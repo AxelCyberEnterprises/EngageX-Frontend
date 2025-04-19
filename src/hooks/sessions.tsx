e5217969d4f2f6852031a3f047ec47b83a76b144
Abdul <abdulmusty03@outlook.com>
2025-04-19 19:24:50 +0100

    refactor: session hooks

--- src/hooks/sessions.tsx
+++ src/hooks/sessions.tsx
@@ -9,76 +9,73 @@ import { useNavigate } from "react-router";
 import { toast } from "sonner";
 
 export function useCreatePublicSpeakingSession() {
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
 
-  return useMutation({
-    mutationKey: ["createPublicSpeakingSession"],
-    mutationFn: async (data: IPOSTSessionPayload) => {
-      return await apiPost<{ id: number }>("/sessions/sessions/", data);
-    },
-    onSuccess: async (data) => {
-      dispatch(closeDialog());
-      navigate(`/sessions/public-speaking-session/${data.id}`);
-    },
-    onError: (error) => {
-      console.error("Error creating public speaking session: ", error);
+    return useMutation({
+        mutationKey: ["createPublicSpeakingSession"],
+        mutationFn: async (data: IPOSTSessionPayload) => {
+            return await apiPost<{ id: number }>("/sessions/sessions/", data);
+        },
+        onSuccess: async (data) => {
+            dispatch(closeDialog());
+            navigate(`/sessions/public-speaking-session/${data.id}`);
+        },
+        onError: (error) => {
+            console.error("Error creating public speaking session: ", error);
 
-      dispatch(closeDialog());
-      toast(
-        <ErrorToast
-          {...{
-            heading: "Error creating session",
-            description:
-              "An error occurred while creating session, please try again.",
-          }}
-        />
-      );
-    },
-  });
+            dispatch(closeDialog());
+            toast(
+                <ErrorToast
+                    {...{
+                        heading: "Error creating session",
+                        description: "An error occurred while creating session, please try again.",
+                    }}
+                />,
+            );
+        },
+    });
 }
 
-export function useCreatePracticeSession({
-  sessionType,
-}: {
-  sessionType: "presentation" | "pitch";
-}) {
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
+export function useCreatePracticeSession({ sessionType }: { sessionType: "presentation" | "pitch" }) {
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
 
-  return useMutation({
-    mutationKey: ["createPitchPracticeSession"],
-    mutationFn: async (data: IPOSTSessionPayload) => {
-      return await apiPost<{ id: number }>(`/sessions/sessions/`, data);
-    },
-    onSuccess: async (data) => {
-      dispatch(closeDialog());
-      navigate(`/sessions/${sessionType}-practice-session/${data.id}`);
-    },
-    onError: (error) => {
-      console.error("Error creating pitch practice session: ", error);
+    return useMutation({
+        mutationKey: ["createPitchPracticeSession"],
+        mutationFn: async (data: IPOSTSessionPayload) => {
+            return await apiPost<{ id: number }>(`/sessions/sessions/`, data);
+        },
+        onSuccess: async (data) => {
+            dispatch(closeDialog());
+            navigate(`/sessions/${sessionType}-practice-session/${data.id}`);
+        },
+        onError: (error) => {
+            console.error("Error creating pitch practice session: ", error);
 
-      dispatch(closeDialog());
-      toast(
-        <ErrorToast
-          {...{
-            heading: "Error creating session",
-            description:
-              "An error occurred while creating session, please try again.",
-          }}
-        />
-      );
-    },
-  });
+            dispatch(closeDialog());
+            toast(
+                <ErrorToast
+                    {...{
+                        heading: "Error creating session",
+                        description: "An error occurred while creating session, please try again.",
+                    }}
+                />,
+            );
+        },
+    });
 }
 
-export function useEndSession(sessionId: string | undefined, duration: any) {
+export function useEndSession(sessionId: string | undefined, duration: any, slidesDuration?: any) {
     const navigate = useNavigate();
 
     return useMutation({
         mutationKey: ["endSession"],
         mutationFn: async () => {
-            await apiPost(`/sessions/sessions-report/${sessionId}/`, { duration: duration });
+            await apiPost(`/sessions/sessions-report/${sessionId}/`, {
+                duration: duration,
+                slide_specific_timing: slidesDuration,
+            });
         },
         onSuccess: () => {
             console.log("Session ended and posted successfully.");
