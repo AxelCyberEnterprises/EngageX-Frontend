d67df7c28d382342b4066ab9e0f8d293e0855053
Abdul <abdulmusty03@outlook.com>
2025-05-04 22:05:33 +0100

    refactor: slide upload

--- src/schemas/media-schema.ts
+++ src/schemas/media-schema.ts
@@ -2,10 +2,15 @@ import { z } from "zod";
 
 const MAX_FILE_SIZE = 20 * 1024 * 1024;
 
+const FileWithIDSchema = z.object({
+    id: z.number(),
+    file: z.instanceof(File),
+});
+
 export const MediaSchema = z
-    .array(z.instanceof(File))
+    .array(FileWithIDSchema)
     .refine((files) => files.length > 0, "File is required.")
     .refine(
-        (files) => files.every((fileWithPreview) => fileWithPreview.size <= MAX_FILE_SIZE),
+        (files) => files.every(({ file }) => file.size <= MAX_FILE_SIZE),
         `File size should not be more than ${MAX_FILE_SIZE / 1024 / 1024}MB`,
     );
