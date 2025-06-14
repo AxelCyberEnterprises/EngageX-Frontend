d93c30b125c10bdbf1af0dd42bfc815fcd3bc584
Abdul <abdulmusty03@outlook.com>
2025-06-14 23:53:21 +0100

    style: public speaking button mobile margin

--- src/components/forms/PublicSpeakingForm/index.tsx
+++ src/components/forms/PublicSpeakingForm/index.tsx
@@ -75,7 +75,7 @@ const PublicSpeakingForm = () => {
                     >
                         Save as Draft
                     </Button>
-                    <div className="mr-25 md:w-fit w-full flex md:flex-row flex-col md:gap-y-0 gap-y-3 items-center md:gap-x-3">
+                    <div className="md:mr-25 md:w-fit w-full flex md:flex-row flex-col md:gap-y-0 gap-y-3 items-center md:gap-x-3">
                         <Button
                             type="button"
                             className="bg-green-sheen hover:bg-green-sheen/80 font-normal md:w-fit w-full md:h-9 h-11 transition"
