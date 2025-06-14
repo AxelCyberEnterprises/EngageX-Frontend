d66808bda7531ee7f1da221c8fb8af959cbd6e2b
Abdul <abdulmusty03@outlook.com>
2025-06-14 23:30:04 +0100

    fix: floating chatbot dashboard toggle

--- src/components/layouts/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout.tsx
@@ -4,45 +4,40 @@ import SideNav from "../dashboard/SideNav";
 import TopNav from "../dashboard/TopNav";
 
 import { ReactNode } from "react";
-import { Toaster } from "../ui/sonner";
 import FloatingChatbot from "../FloatingChatbot";
-import { useDispatch } from "react-redux";
-import { hide } from "@/store/slices/floatingChatbotSlice";
+import { Toaster } from "../ui/sonner";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
-  const dispatch = useDispatch();
-  return (
-    <div onClick={() => dispatch(hide())}>
-      <SidebarProvider
-        style={
-          {
-            "--sidebar-width": "18rem",
-            "--sidebar-width-mobile": "21rem",
-          } as React.CSSProperties
-        }
-      >
-        <div className="flex h-screen w-screen">
-          {/* Sidebar */}
-          <SideNav />
+    return (
+        <div>
+            <SidebarProvider
+                style={
+                    {
+                        "--sidebar-width": "18rem",
+                        "--sidebar-width-mobile": "21rem",
+                    } as React.CSSProperties
+                }
+            >
+                <div className="flex h-screen w-screen">
+                    {/* Sidebar */}
+                    <SideNav />
 
-          {/* Main Content Area */}
-          <SidebarInset className="flex flex-1 flex-col overflow-y-hidden">
-            {/* Top Nav with Breadcrumbs */}
-            <TopNav />
+                    {/* Main Content Area */}
+                    <SidebarInset className="flex flex-1 flex-col overflow-y-hidden">
+                        {/* Top Nav with Breadcrumbs */}
+                        <TopNav />
 
-            {/* Main Content Area with Outlet */}
-            <div className="flex-1 overflow-y-auto scrollbar-hide">
-              {children}
-              <Toaster
-                toastOptions={{ classNames: { content: "size-full" } }}
-              />
-            </div>
-            <FloatingChatbot />
-          </SidebarInset>
+                        {/* Main Content Area with Outlet */}
+                        <div className="flex-1 overflow-y-auto scrollbar-hide">
+                            {children}
+                            <Toaster toastOptions={{ classNames: { content: "size-full" } }} />
+                        </div>
+                        <FloatingChatbot />
+                    </SidebarInset>
+                </div>
+            </SidebarProvider>
         </div>
-      </SidebarProvider>
-    </div>
-  );
+    );
 };
 
 export default DashboardLayout;
