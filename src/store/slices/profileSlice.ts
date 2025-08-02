58b0c144fa79cee903df8e227e463b0eb2da0c4b
Abdul <abdulmusty03@outlook.com>
2025-08-02 14:22:52 +0100

    chore: implement enterprise dashboards

--- src/store/slices/profileSlice.ts
+++ src/store/slices/profileSlice.ts
@@ -1,24 +1,25 @@
+import { UserProfile } from "@/hooks/settings";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface ProfileState {
-  data: any | null; // Or strongly type this if you have a profile interface
+    data: UserProfile | null; // Or strongly type this if you have a profile interface
 }
 
 const initialState: ProfileState = {
-  data: null,
+    data: null,
 };
 
 const profileSlice = createSlice({
-  name: "profile",
-  initialState,
-  reducers: {
-    setProfile: (state, action: PayloadAction<any>) => {
-      state.data = action.payload;
+    name: "profile",
+    initialState,
+    reducers: {
+        setProfile: (state, action: PayloadAction<UserProfile>) => {
+            state.data = action.payload;
+        },
+        clearProfile: (state) => {
+            state.data = null;
+        },
     },
-    clearProfile: (state) => {
-      state.data = null;
-    },
-  },
 });
 
 export const { setProfile, clearProfile } = profileSlice.actions;
