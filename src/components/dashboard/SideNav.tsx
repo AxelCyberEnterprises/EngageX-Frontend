ebb671677cbc3d5223750055815064d896b4064e
Abdul <abdulmusty03@outlook.com>
2025-11-13 12:00:38 +0100

    chore: remove console logs

--- src/components/dashboard/SideNav.tsx
+++ src/components/dashboard/SideNav.tsx
@@ -83,14 +83,11 @@ const SideNav: React.FC = () => {
             if (!isEnterpriseUser) return true;
             const pathSegments = path.split("/").filter(Boolean);
             const lastSegment = pathSegments[pathSegments.length - 1];
-            console.log("Last segment: ", lastSegment);
 
             const matchedVertical = Object.entries(verticalToPathMap).find(([, slug]) => slug === lastSegment)?.[0];
-            console.log("Matched vertical: ", matchedVertical);
 
             if (!matchedVertical) return true;
 
-            console.log("Access: ", accessibleVerticals.includes(matchedVertical));
             return accessibleVerticals.includes(matchedVertical);
         },
         [accessibleVerticals, isEnterpriseUser, verticalToPathMap],
